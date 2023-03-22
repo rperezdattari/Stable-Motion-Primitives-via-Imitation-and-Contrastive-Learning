dead5184a3282d5032d5510567f4f0377140e91b
Rodrigo <rodrigoperezd@gmail.com>
2023-03-22 15:34:13 +0100

    Change default

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 # Get arguments
 parser = ArgumentParser()
-parser.add_argument('--params', type=str, default='2nd_order_2D', help='')
+parser.add_argument('--params', type=str, default='1st_order_2D', help='')
 parser.add_argument('--results-base-directory', type=str, default='./', help='')
 args = parser.parse_args()
 
